cb4d224964ee757d832c3d329d4f59d4a59c520b
nabilmahtout-web <nabilmahtout@gmail.com>
2026-07-04 04:31:54 +0200

    Refactor asset paths in sw.js
    Updated asset paths in service worker to remove 'icons/' prefix.

--- sw.js
+++ sw.js
@@ -3,12 +3,12 @@ const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-192.png",
-  "./icons/icon-maskable-512.png",
-  "./icons/apple-touch-icon.png",
-  "./icons/favicon-32.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable-192.png",
+  "./icon-maskable-512.png",
+  "./apple-touch-icon.png",
+  "./favicon-32.png",
 ];
 
 self.addEventListener("install", (event) => {
